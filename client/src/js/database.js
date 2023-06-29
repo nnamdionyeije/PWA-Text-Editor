ad1ab3bdfc148f5601f967c298f5aea9ee47e942
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-06-29 16:18:52 -0400

    to start it up

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -21,28 +21,23 @@ export const putDb = async (content) => {
   const request = store.put({content: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
-}; //
+}; 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
-
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
-
   // Create a new transaction and specify the database and data privileges.
   const tx = jateDb.transaction('jate', 'readonly');
-
   // Open up the desired object store.
   const store = tx.objectStore('jate');
-
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
-
+  const request = store.get(0);
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
   return result;
-}; //console.error('getDb not implemented')
+}; 
 
 initdb();
